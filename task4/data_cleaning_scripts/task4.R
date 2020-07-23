a1d622ba0f8bba0542f5917db531a3d13babfa7e
Steve Maddison <stevemaddison@steves-mbp.home>
2020-07-23 19:30:26 +0100

    Question 2 completed

--- task4/data_cleaning_scripts/task4.R
+++ task4/data_cleaning_scripts/task4.R
@@ -6,10 +6,12 @@ library(here)
 library(readr)
 library(readxl)
 
+here()
+
 # read raw data files
-candy_2015 <- read_excel("../raw_data/boing-boing-candy-2015.xlsx")
-candy_2016 <- read_excel("../raw_data/boing-boing-candy-2016.xlsx")
-candy_2017 <- read_excel("../raw_data/boing-boing-candy-2017.xlsx")
+candy_2015 <- read_excel(here("/raw_data/boing-boing-candy-2015.xlsx"))
+candy_2016 <- read_excel(here("/raw_data/boing-boing-candy-2016.xlsx"))
+candy_2017 <- read_excel(here("/raw_data/boing-boing-candy-2017.xlsx"))
 
 # select required columns by position (column numbers)
 candy_2015_clean <- candy_2015 %>%
